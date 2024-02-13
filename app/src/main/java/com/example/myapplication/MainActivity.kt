b2df4aa563c5ac4988d90ad9dbc1c3d457be1c19
MavRicK13 <aaddiwate@gmail.com>
2024-02-13 12:24:51 +0530

    sdfa

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.reflect.Modifier
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener{
+
+        }
+
+    }
+}
